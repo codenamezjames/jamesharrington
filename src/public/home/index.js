64c5e8e848ce16115fa61af820c909bc484e1b6d
James Harrington <indoisfun@gmail.com>
2017-03-09 16:59:47 -0600

    email working

--- src/public/home/index.js
+++ src/public/home/index.js
@@ -7,13 +7,12 @@ export class index {
     constructor(){
         this.projects = projects
     }
-    submitContact(form){
-        console.log(form)
-        // client.post('http://64.137.217.6:3000/mail', this.contact)
-        //   .then((d) => {
-        //       console.log(d)
-        //       this.contact = {}
-        //       console.log(this)
-        //   })
+    submitContact(){
+        client.post('http://64.137.217.6:3000/mail', this.contact)
+          .then((d) => {
+              console.log(d)
+              this.contact = {}
+              console.log(this)
+          })
     }
 }
