313fdb61146e6e1ec529b078d5a1cbdebcf5730f
James Harrington <jamesdonaldharrington@gmail.com>
2018-05-28 10:38:30 -0400

    updating some projects

--- src/config/projects.js
+++ src/config/projects.js
@@ -70,5 +70,35 @@ export default [
         ],
         blurb: 'An angular 1.X service that pulls info from a Google Sheets spread sheet',
         discription: 'Laboris nulla aliqua velit aute nostrud dolor pariatur sunt do deserunt qui. Eu sunt eiusmod in eu laboris duis cillum. Est elit labore pariatur excepteur nulla in tempor cillum. Sunt mollit deserunt quis nisi consectetur laborum elit id enim. Anim eu amet consequat voluptate consectetur nostrud. Enim ut cupidatat mollit dolore aliquip ex sunt ipsum officia.'
+    },
+    {
+        title: 'Jlib Proto extention',
+        images: [
+            {
+                src: '/portfolio/jlib/screen-shot-1.png',
+                alt: 'Jlib readme screen shot'
+            },
+            {
+                src: '/portfolio/jlib/screen-shot-2.png',
+                alt: 'Jlib code screen shot'
+            }
+        ],
+        blurb: 'A simle standalone library for extending prototypes objects without dirting the proto object',
+        discription: 'jlib is a easily extendable library for dealing with data in a different way. The mentality of jlib is extending a subjects prototype with out dirtying its actual prot object'
+    },
+    {
+        title: 'Custom Template Language',
+        images: [
+            {
+                src: '/portfolio/chicos-template-lang/screen-shot-1.png',
+                alt: 'chicos-template-lang readme screen shot'
+            },
+            {
+                src: '/portfolio/chicos-template-lang/screen-shot-2.png',
+                alt: 'chicos-template-lang code screen shot'
+            }
+        ],
+        blurb: 'A hevaly tested template language for simple prototyping',
+        discription: 'This custom template language was designed for a client to make it possible to quickly prototype simple layouts with limited knolage of html and css.'
     }
 ]
